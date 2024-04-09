c37f150d369d79935325e6ad4c5890ef2b8fe241
Lorenzo Greco <lorenzodgreco@hotmail.com>
2024-04-09 12:26:54 -0300

    subo proyecto vacio

---
+++ src/main/java/canlor/tp1robots/HelloController.java
@@ -0,0 +1,14 @@
+package canlor.tp1robots;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module canlor.tp1robots {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens canlor.tp1robots to javafx.fxml;
+    exports canlor.tp1robots;
+}
