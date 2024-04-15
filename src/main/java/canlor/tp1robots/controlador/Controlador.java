13ec8db5314fbb4ace41df58ace821c95eb810d6
cpiccin <cpiccin@fi.uba.ar>
2024-04-15 19:22:21 -0300

    maneja evento de tpSeguro y excepciones en setRedimensionar

--- src/main/java/canlor/tp1robots/controlador/Controlador.java
+++ src/main/java/canlor/tp1robots/controlador/Controlador.java
@@ -8,10 +8,12 @@ public class Controlador {
     private final Juego modelo;
     private final RobotsView vista;
     private Eventos eventos;
+    private boolean tpSeguroActivado;
 
     public Controlador(Juego modelo, RobotsView vista){
         this.modelo = modelo;
         this.vista = vista;
+        tpSeguroActivado = false;
     }
 
     public void iniciar() {
@@ -25,14 +27,13 @@ public class Controlador {
             try {
                 int valor1 = Integer.parseInt(espacio[0]);
                 int valor2 = Integer.parseInt(espacio[1]);
-
-                if (valor1 >= 25 && valor2 >= 25) {
-                    modelo.redimensionar(Integer.parseInt(espacio[0]), Integer.parseInt(espacio[1]));
-                    modelo.reiniciar();
-                    vista.redimensionar();
-                } else {
-                    vista.setErrorLabel("Ingrese numeros >=25");
+                if (valor1 <= 0 || valor2 <= 0) {
+                    vista.setErrorLabel("Ingrese valores mayores a 0");
+                    return;
                 }
+                modelo.redimensionar(Integer.parseInt(espacio[0]), Integer.parseInt(espacio[1]));
+                modelo.reiniciar();
+                vista.redimensionar();
             } catch (NumberFormatException e) {
                 vista.setErrorLabel("Ingrese valores numéricos");
             }
@@ -44,10 +45,10 @@ public class Controlador {
             vista.actualizar();
         });
 
-        /*eventos.setTpSeguro(event -> {
-            modelo.TpSeguro();
-            vista.actualizar();
-        });*/
+
+        eventos.setTpSeguro(_ -> {
+            tpSeguroActivado = true;
+        });
 
         eventos.setEsperar(_ -> {
             modelo.mover(modelo.getJugador().getX(), modelo.getJugador().getY());
@@ -62,11 +63,14 @@ public class Controlador {
         eventos.setMouseClick(e -> {
             int columna = (int) (e.getX() / 16);
             int fila = (int) (e.getY() / 16);
-
-            modelo.mover(fila, columna);
+            if (tpSeguroActivado) {
+                modelo.TpSeguro(fila, columna);
+                tpSeguroActivado = false;
+            } else {
+                modelo.mover(fila, columna);
+            }
             vista.actualizar();
         });
-
 
         vista.crearEventos(eventos);
     }
